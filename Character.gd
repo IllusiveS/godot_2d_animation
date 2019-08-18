0f5562ea719aee02d162e58eeebc0a464e1d938d
Wysocki Patryk <wysockipawelpatryk@gmail.com>
2019-08-18 15:44:18 +0200

    anims

---
+++ Character.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+class_name Character
+
+export var speed : int
+export var turn_speed : float
+
+onready var animation_tree = $AnimationTree
+var target_direciton : Vector2
+var direction : Vector2
+var h_flip = 1
+
+var horizontal_update_coroutine;
+
+func _ready():
+	target_direciton = Vector2()
+	direction = Vector2()
+	horizontal_update_coroutine = flip_horizontal()
+	pass # Replace with function body.
+
+func set_move_target(target : Vector2):
+	target_direciton = target * speed
+	pass
+
+func _process(delta):
+	horizontal_update_coroutine.resume(delta)
+	target_direciton = move_and_slide(target_direciton)
+	pass
+
+func flip_horizontal():
+	var delta = yield()
+	if direction.x > 0:
+		h_flip += turn_speed * delta
+		if h_flip > 1:
+			h_flip = 1
+	
+	if direction.x < 0:
+		h_flip -= turn_speed * delta
+		if h_flip < -1:
+			h_flip = -1
+	
+	animation_tree["parameters/Flip/blend_position"] = h_flip
+	yield()
+	pass
---
+++ CharacterController.gd
@@ -0,0 +1,27 @@
+extends Node
+
+export var character_controlled : NodePath
+var character_to_control : Character
+
+var velocity = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	character_to_control = get_node(character_controlled)
+	pass # Replace with function body.
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	velocity = velocity.normalized()
+
+
+func _process(delta):
+	pass
