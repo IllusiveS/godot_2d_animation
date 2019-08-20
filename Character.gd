be90bda1c1b785b713a0692f476f468c12bdda53
Wysocki Patryk <wysockipawelpatryk@gmail.com>
2019-08-20 23:42:49 +0200

    before anim fix

--- Character.gd
+++ Character.gd
@@ -9,6 +9,7 @@ onready var animation_tree = $AnimationTree
 var target_direciton : Vector2
 var direction : Vector2
 var h_flip = 1
+var target_h_flip = 1
 
 func _ready():
 	target_direciton = Vector2()
@@ -21,20 +22,28 @@ func set_move_target(target : Vector2):
 
 func _process(delta):
 	flip_horizontal(delta)
-	target_direciton = move_and_slide(target_direciton)
+	#target_direciton = move_and_slide(target_direciton)
+	direction = target_direciton
 	pass
 
 func flip_horizontal(delta):
-	if direction.x >= 0:
+	if direction.x > 0:
+		target_h_flip = -1
+	
+	if direction.x < 0:
+		target_h_flip = 1
+	
+	if target_h_flip == 1:
 		h_flip += turn_speed * delta
 		if h_flip > 1:
 			h_flip = 1
 	
-	if direction.x < 0:
+	if target_h_flip == -1:
 		h_flip -= turn_speed * delta
 		if h_flip < -1:
 			h_flip = -1
 	
-	animation_tree["parameters/Flip/blend_position"] = h_flip
-	yield()
+	#print(global_scale.x)
+	
+	$"..".scale.x = h_flip
 	pass
--- CharacterController.gd
+++ CharacterController.gd
@@ -12,13 +12,13 @@ func _ready():
 
 func get_input():
 	velocity = Vector2()
-	if Input.is_action_pressed('right'):
+	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
-	if Input.is_action_pressed('left'):
+	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
-	if Input.is_action_pressed('down'):
+	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
-	if Input.is_action_pressed('up'):
+	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
 	velocity = velocity.normalized()
 	character_to_control.set_move_target(velocity)
